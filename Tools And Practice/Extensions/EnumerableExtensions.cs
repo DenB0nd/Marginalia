ab4204db462439a37cbce99ca50b1919f9d6bc7b
Admin <dbondarenko066@mail.ru>
2022-05-17 03:03:43 +0300

    extensions separated

--- Tools And Practice/Extensions/EnumerableExtensions.cs
+++ Tools And Practice/Extensions/EnumerableExtensions.cs
@@ -2,47 +2,6 @@
 
 public static class EnumerableExtensions
 {
-    public static T[,] JaggedToMultidimensional<T>(this T[][] jaggedArray)
-    {
-        int rows = jaggedArray.Length;
-        int columns = jaggedArray.Max(subArray => subArray.Length);
-
-        T[,] twoDimensionalArray = new T[rows, columns];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < jaggedArray[i].Length; j++)
-            {
-                twoDimensionalArray[i, j] = jaggedArray[i][j];
-            }
-        }
-
-        return twoDimensionalArray;
-    }
-
-    public static T[][] TwoDimensionalToJagged<T>(this T[,] twoDimensionalArray)
-    {
-        int rowsFirstIndex = twoDimensionalArray.GetLowerBound(0);
-        int rowsLastIndex = twoDimensionalArray.GetUpperBound(0);
-        int numberOfRows = rowsLastIndex - rowsFirstIndex + 1;
-
-        int columnsFirstIndex = twoDimensionalArray.GetLowerBound(1);
-        int columnsLastIndex = twoDimensionalArray.GetUpperBound(1);
-        int numberOfColumns = columnsLastIndex - columnsFirstIndex + 1;
-
-        T[][] jaggedArray = new T[numberOfRows][];
-        for (int i = 0; i < numberOfRows; i++)
-        {
-            jaggedArray[i] = new T[numberOfColumns];
-
-            for (int j = 0; j < numberOfColumns; j++)
-            {
-                jaggedArray[i][j] = twoDimensionalArray[i + rowsFirstIndex, j + columnsFirstIndex];
-            }
-        }
-
-        return jaggedArray;
-    }
-
     public static IEnumerable<T> GetColumn<T>(this T[,] matrix, int columnNumber)
     {
         ArgumentNullException.ThrowIfNull(matrix, nameof(matrix));
@@ -86,6 +45,7 @@ public static class EnumerableExtensions
     public static string ConvertToString<T>(this IEnumerable<T> enumerable, string separator = "")
     {
         ArgumentNullException.ThrowIfNull(enumerable);
+        ArgumentNullException.ThrowIfNull(separator);
 
         return string.Join(separator, enumerable.Select(x => x?.ToString()));
     }
