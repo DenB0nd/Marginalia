6a2dad6654f3f4b37e737d148e84c4e70b8fa435
DenB0nd <dbondarenko066@mail.ru>
2022-12-18 06:47:34 +0300

    Combinatorics

--- Tools And Practice/Algorithms/MathAlgorithms.cs
+++ Tools And Practice/Algorithms/MathAlgorithms.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Numerics;
 
 namespace Marginalia.Algorithms;
 
@@ -116,4 +117,81 @@ public static class MathAlgorithms
         return Math.Abs(res) / 2;
     }
 
+    public static BigInteger Factorial(int number)
+    {
+        if(number <= 0)
+        {
+            return 1;
+        }
+
+        BigInteger res = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            res *= i;
+        }
+
+        return res;
+    }
+
+    public static BigInteger Combination(int n, int k)
+    {
+        BigInteger res = 1;
+        for(int i = k + 1; i <= n; i++)
+        {
+            res *= i;
+        }
+        return res / Factorial(k - 1);
+    }
+
+    public static BigInteger Permution(int n, int k)
+    {
+        BigInteger res = 1;
+        for (int i = k; i <= n; i++)
+        {
+            res *= i;
+        }
+        return res;
+    }
+
+    // taken from https://www.nayuki.io/page/fast-fibonacci-algorithms
+    public static BigInteger Fibonacci(int n)
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+        BigInteger a = BigInteger.Zero;
+        BigInteger b = BigInteger.One;
+        for (int i = 31; i >= 0; i--)
+        {
+            BigInteger d = a * (b * 2 - a);
+            BigInteger e = a * a + b * b;
+            a = d;
+            b = e;
+            if ((((uint)n >> i) & 1) != 0)
+            {
+                BigInteger c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        return a;
+    }
+
+    // (2n)! / (n + 1)!(n)!
+    public static BigInteger Catalan(int n)
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+
+        BigInteger res = 1;
+        for (int i = n + 1; i <= 2 * n; i++)
+        {
+            res *= i;
+        }
+
+        return res / Factorial(n + 1);
+    }
 }
--- Tools And Practice/Program.cs
+++ Tools And Practice/Program.cs
@@ -1,8 +1,14 @@
-﻿using Marginalia.Extensions;
-using Marginalia.Algorithms;
+﻿using Marginalia.Algorithms;
+using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Numerics;
 
 Point[] points = { new Point(3, 4), new Point(5, 11), new Point(12, 8), new Point(9, 5), new Point(5, 6) };
 
-Console.WriteLine(MathAlgorithms.ShoelaceFormula(points));
+Stopwatch sw = Stopwatch.StartNew();
+BigInteger num = MathAlgorithms.Catalan(100000);
+sw.Stop();
+Console.WriteLine(num);
+Console.WriteLine(sw.ElapsedMilliseconds);
+
